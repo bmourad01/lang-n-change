0b5d846fa85822ae9853d2c45eca473bedfd3a2f
bmourad01 <Benjamin_Mourad@student.uml.edu>
2020-09-23 11:07:23 -0400

    improve lprolog.sh helper script

--- lprolog.sh
+++ lprolog.sh
@@ -1,10 +1,20 @@
 #!/bin/sh
 
-DIR="./lprolog/"
+BASEDIR="./lprolog/"
 FILE=$1
 BASENAME="$(basename ${FILE})"
+MODNAME="${BASENAME%%.lan}"
+DIR="${BASEDIR}${MODNAME}/"
 
+# create the directories
+mkdir -p $BASEDIR
 mkdir -p $DIR
 
-dune exec bin/lprolog_sig.exe $FILE > "${DIR}${BASENAME%%.lan}.sig"
-dune exec bin/lprolog_mod.exe $FILE > "${DIR}${BASENAME%%.lan}.mod"
+# compile the signature and module
+dune exec bin/lprolog_sig.exe $FILE > "${DIR}${MODNAME}.sig"
+dune exec bin/lprolog_mod.exe $FILE > "${DIR}${MODNAME}.mod"
+
+CWD=$(pwd)
+cd $DIR
+# compile the actual program with Teyjus
+(tjcc $MODNAME && tjlink $MODNAME); cd $CWD
